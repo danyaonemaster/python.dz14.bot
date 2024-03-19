69cf1b719aaf91f1c1b173ab468d07820c775227
danyaonemaster <157752827+danyaonemaster@users.noreply.github.com>
2024-03-19 16:11:00 +0200

    added tele_bot.py, changes_log.py

--- tele_bot.py
+++ tele_bot.py
@@ -4,6 +4,9 @@ import random
 import telebot
 from telebot import types
 
+from changes_log import dictionary
+from changes_log import sticker_list
+
 bot = telebot.TeleBot("6821622670:AAFa0ZfrVgFtUsUYRcTldnJdwwi38B4R3gk")
 day_list = calendar.day_name
 month_list = calendar.month_name
@@ -35,34 +38,13 @@ def func(message):
                                               f"{day_list[random_num - 1]}")
         case "randon month":
             random_num = random.randint(1, 12)
-            if random_num in [12, 1, 2]:
-                bot.send_message(message.chat.id, f"month: {month_list[random_num]}\n"
-                                                  f"season: winter")
-            elif random_num in [3, 4, 5]:
-                bot.send_message(message.chat.id, f"month: {month_list[random_num]}\n"
-                                                  f"season: spring")
-            elif random_num in [6, 7, 8]:
-                bot.send_message(message.chat.id, f"month: {month_list[random_num]}\n"
-                                                  f"season: summer")
-            else:
-                bot.send_message(message.chat.id, f"month: {month_list[random_num]}\n"
-                                                  f"season: autumn")
+            bot.send_message(message.chat.id, f"month: {month_list[random_num]}\n"
+                                              f"season: {dictionary.get(random_num)}")
         case "randon num":
             random_num = str(random.randint(101, 999))
             bot.send_message(message.chat.id, random_num[::-1])
         case "randon stick":
-            sticker_list = ["CAACAgQAAxkBAAICTGX4oj1EVy5zATCn2lw4PQUPz7vhAAKaFwACpvFxHs9ZRtDBeIxDNAQ",
-                            "CAACAgIAAxkBAAICTmX4omw6E3tqrOMZMTRvMeqBOpcqAAJpHgACHe1hSSl1ko95EAL",
-                            "CAACAgIAAxkBAAICUGX4opYgXb8ygnxrH1kIDuw-R_Z8AAJ-GQAC9LigSW1WKDwgc2U_NAQ",
-                            "CAACAgIAAxkBAAICVGX4otwTz9syfFWnJJZ5r1JxU84UAAKLFQACvYzxS0ySmLTCdQV6NAQ",
-                            "CAACAgIAAxkBAAICVmX4owZCjf90HKjR1svvSlbqs_StAAKeFwACpmbpSeMeZ7i4jpMoNAQ",
-                            "CAACAgIAAxkBAAICWGX4oxhFbnARS8-bPsPYJbFJWebxAAKeFwACnbzhSQbQcN_mxpdJNAQ",
-                            "CAACAgIAAxkBAAICWmX4ozltlpWAPwMBbPVDYA6Swx_SAALvJAACfy2RSN7sKkUmnd2WNAQ",
-                            "CAACAgIAAxkBAAICXGX4o1ZZnplv_TVIfhKLsFiOCvwXAAJTFgAC_ONBSY5DRFRhEBTWNAQ",
-                            "CAACAgIAAxkBAAICXmX4o4MF5B0uEuWaL87cKR6Ow5gqAALgHAACdtBxS_KzA-uoogSDNAQ",
-                            "CAACAgIAAxkBAAICYGX4o5JyStdN2jt9G1DGLmgwgtVrAAIiEwACMZVBScO6foNAo9ZBNAQ"]
             bot.send_sticker(message.chat.id, random.choice(sticker_list))
 
 
 bot.infinity_polling()
-json=json.dumps(1)
