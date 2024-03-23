284231ca4059494d71019df640f398d645da7df8
danyaonemaster <157752827+danyaonemaster@users.noreply.github.com>
2024-03-23 17:17:47 +0200

    added tele_bot.py

--- tele_bot.py
+++ tele_bot.py
@@ -8,38 +8,52 @@ from changes_log import dictionary
 from changes_log import sticker_list
 
 bot = telebot.TeleBot("6821622670:AAFa0ZfrVgFtUsUYRcTldnJdwwi38B4R3gk")
-day_list = calendar.day_name
-month_list = calendar.month_name
 
 
 @bot.message_handler(commands=['start'])
-def start_message(message):
-    bot.send_message(message.chat.id, "<b><em>Ha Ti Lox!!!</em></b> \U0001F595",
-                     parse_mode='html')
+def send_welcome(message):
+    bot.reply_to(message,
+                 "Hi! Enter /day to find out the day weeks or"
+                 " /month to find out the month and time of year or"
+                 " /button to display the keyboard")
 
 
 @bot.message_handler(commands=['button'])
 def button_message(message):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    item1 = types.KeyboardButton("randon day")
-    item2 = types.KeyboardButton("randon month")
-    item3 = types.KeyboardButton("randon num")
-    item4 = types.KeyboardButton("randon stick")
-    markup.add(item1, item2, item3, item4)
-    bot.send_message(message.chat.id, 'Выберите что вам надо', reply_markup=markup)
+    item1 = types.KeyboardButton("randon num")
+    item2 = types.KeyboardButton("randon stick")
+    markup.add(item1, item2)
+    bot.send_message(message.chat.id, 'Choose what you need', reply_markup=markup)
+
+
+@bot.message_handler(commands=['day'])
+def send_message(message):
+    send_day = bot.reply_to(message, "Hello! Enter a number from 1 to 7")
+    send_day = bot.register_next_step_handler(send_day, day_name)
+
+
+def day_name(message):
+    day_number = int(message.text)
+    bot.reply_to(message, f"number: {day_number}\n "
+                          f"{calendar.day_name[day_number - 1]}")
+
+
+@bot.message_handler(commands=['month'])
+def send_mouth(message):
+    send_month = bot.reply_to(message, "Hello! Enter a number from 1 to 12")
+    send_month = bot.register_next_step_handler(send_month, month_name)
+
+
+def month_name(message):
+    month_number = int(message.text)
+    bot.reply_to(message, f"season: {dictionary.get(month_number)}\n "
+                          f"month: {calendar.month_name[month_number]}.")
 
 
 @bot.message_handler(content_types=["text"])
 def func(message):
     match message.text.lower():
-        case "randon day":
-            random_num = random.randint(1, 7)
-            bot.send_message(message.chat.id, f"randon num :{random_num}\n"
-                                              f"{day_list[random_num - 1]}")
-        case "randon month":
-            random_num = random.randint(1, 12)
-            bot.send_message(message.chat.id, f"month: {month_list[random_num]}\n"
-                                              f"season: {dictionary.get(random_num)}")
         case "randon num":
             random_num = str(random.randint(101, 999))
             bot.send_message(message.chat.id, random_num[::-1])
